f326152605a11dd2b031454d73ab7ce8c444415b
Alex Aiezza <axa9070@rit.edu>
2015-04-22 10:46:36 -0400

    756 - Project 3 - Initial Commit
    All setup in eclipse to start coding the service

---
+++ 756/project3/index.php
@@ -0,0 +1,35 @@
+<?php
+?>
+
+<!DOCTYPE HTML>
+
+<html lang="EN">
+<head>
+<meta charset="UTF-8">
+
+<title>Beer Service | WSDL</title>
+<meta name="description" content="basic">
+<meta name="author" content="Alex Aiezza">
+
+<link type="text/css" rel="stylesheet" href="css/style.css">
+
+<link rel="icon" type="image/ico" href="images/favicon.ico">
+
+</head>
+
+<body>
+	<div id="content">
+		<p>
+			The code for the Beer Service
+			can be found in this directory on the server.
+		</p>
+		<p>The <a href="../homework/05">client</a> built for Homework 5 (Week 10) can be used to access this
+			service.</p>
+		<hr>
+		<p>
+		  The Java docs for this project can be found <a href="docs">here</a>.
+		</p>
+	</div>
+</body>
+
+</html>
